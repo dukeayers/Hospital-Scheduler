2abd8f56321d0c55d46c83a7b8bf73aa9c2fc91b
Duke Ayers <Dfa24@nau.edu>
2014-11-23 15:07:33 -0700

    Fixed Session Login

--- php/SessionLogin.php
+++ php/SessionLogin.php
@@ -1,9 +1,8 @@
 <?php
 session_start();
-define("HOST", "localhost"); // The host you want to connect to.
-define("USER", "findfuut_nau"); // The database username.
-define("PASSWORD", "=naugroup"); // The database password.
-define("DATABASE", "findfuut_scheduler");
-
+define("HOST", "localhost");
+define("USER", "root"); // The database username.
+define("PASSWORD", "root"); // The database password.
+define("DATABASE", "p3_scheduler");
 $mysqli = new mysqli(HOST, USER, PASSWORD, DATABASE)
 or die("Failed to connect");
